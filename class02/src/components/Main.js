101aa3d4c779c39f656c096fb2255b6a722fedf6
Curtrick Walton <curtrickw@yahoo.com>
2022-08-09 15:45:44 -0500

    did some code-along

--- class02/src/components/Main.js
+++ class02/src/components/Main.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import data from '../data.json';
+import Product from './Product'
 
 export default class Main extends Component {
   render() {
     return (
-      <div>Main</div>
+      <div>
+        <h1>Some Products can found here</h1>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/640px-Banana-Single.jpg' alt='A Banana'></img>
+     {/* product component goes here  */}
+     {data.products.map(product => {
+          return(
+          <Product title={product.title} description={product.description} img={product.images[0]} key={product.id}/>
+          )
+        })}
+      </div>
     )
   }
 }
--- class02/src/components/Product.js
+++ class02/src/components/Product.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
 
+import{Button, Card} from 'react-bootstrap'
+
 export default class Product extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cart: 0,
+    }
+  }
+  handleClicks(){
+    // When user clicks
+    // add 1
+    this.setState((prevState) => ({
+      cart: prevState.cart + 1
+    }))
+    console.log(this.state.cart); // console is syncc=rhonous
+  }
+
   render() {
     return (
-      <div>Product</div>
+      <div>
+        {/* {I will pass my data in this component, so I I need to use this.props here} */}
+        {/* <h1>{this.props.title}</h1>
+        <h3>🛒 {this.state.cart} added to cart</h3>
+        <img onClick={() => this.handleCLicks()} src={this.props.img} alt={this.props.title}></img>
+        <p>{this.props.description}</p> */}
+
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.props.img} alt={this.props.title}/>
+        <Card.Body>
+        <Card.Title>{this.props.title}</Card.Title>
+        <Card.Text>
+          {this.props.description}
+        </Card.Text>
+        <Button onClick={() => this.handleClicks()} variant="primary">🛒 {this.state.cart} added to cart</Button>
+      </Card.Body>
+        </Card>
+        
+      </div>
     )
   }
 }
